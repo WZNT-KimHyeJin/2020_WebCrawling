238f5b7d3688e65f955126115ef1320f4c9ad259
김몜몌 <62338783+WZNT-KimHyeJin@users.noreply.github.com>
2020-11-07 00:42:48 +0900

    돌아갔지롱

--- node.js/main.js
+++ node.js/main.js
@@ -1,21 +1,19 @@
 var http = require('http');
 var fs = require('fs');
 var url = require('url');
-//require : 사용자가 http, fs, url을 요구한다는 의미
-//url이라는 modul을 사용할 것임.
 
 var app = http.createServer(function(request,response){
-  var _url = request.url;
-  var queryData = url.parse(_url,true).query;
-  // console.log(queryData.name);
-  console.log("이 값을 봐야하네 : "+_url); // url을 분석해서 데이터를 추출 해야 함.
+    var _url = request.url;
+    var queryData = url.parse(_url,true).query;
+    console.log("이 값을 봐야하네 : "+_url);
+    console.log(queryData.id);
     if(_url == '/'){
       _url = '/index.html';
     }
-    if(url == '/favicon.ico'){
-        response.writeHead(404);
-        response.end();
-        return;
+    if(_url == '/favicon.ico'){
+      response.writeHead(404);
+      response.end();
+      return;
     }
     response.writeHead(200);
     response.end(fs.readFileSync(__dirname + _url));
